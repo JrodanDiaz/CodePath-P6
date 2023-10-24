ff52848f766169a372d676604976ec981b2adc01
Jordan <jordancdiaz04@gmail.com>
2023-10-24 02:43:52 -0400

    fix CORS bug by preventing refresh

--- src/components/QueryData.jsx
+++ src/components/QueryData.jsx
@@ -6,21 +6,24 @@ export default function QueryData({ title, query, setQuery }) {
   const [input, setInput] = useState("");
 
   const handleChange = (e) => {
+    // e.preventDefault();
     setInput(e.target.value);
   };
 
-  const handleSubmit = () => {
+  const handleSubmit = (e) => {
+    e.preventDefault();
     setQuery(input);
+    setInput("");
   };
 
   return (
     <>
       <div className="query-data-wrapper">
         <div className="query-data-container">
-          <h1>Current {title}</h1>
+          <h1 className="query">Current {title}</h1>
           <h3>{query}</h3>
         </div>
-        <form onSubmit={handleSubmit}>
+        <form onSubmit={(e) => handleSubmit(e)}>
           <input
             type="text"
             placeholder={`Enter ${title}`}
